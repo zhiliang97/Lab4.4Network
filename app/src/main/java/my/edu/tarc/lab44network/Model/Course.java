0013d1b676fd3c87c928cf4ae74bad3702ba7062
zhiliang97 <chowzhiliang@hotmail.com>
2017-12-19 15:56:53 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab44network/Model/Course.java
@@ -0,0 +1,53 @@
+package my.edu.tarc.lab44network.Model;
+
+/**
+ * Created by TAR UC on 7/31/2017.
+ */
+
+public class Course {
+    private String code;
+    private String title;
+    private String credit;
+
+    public Course() {
+    }
+
+    public Course(String code, String title, String credit) {
+        this.code = code;
+        this.title = title;
+        this.credit = credit;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCredit() {
+        return credit;
+    }
+
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "code='" + code + '\'' +
+                ", title='" + title + '\'' +
+                ", credit='" + credit + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/my/edu/tarc/lab44network/Model/CourseAdapter.java
@@ -0,0 +1,44 @@
+package my.edu.tarc.lab44network.Model;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import my.edu.tarc.lab44network.R;
+
+/**
+ * Created by TARC on 8/6/2015.
+ */
+public class CourseAdapter extends ArrayAdapter<Course> {
+
+    public CourseAdapter(Activity context, int resource, List<Course> list) {
+        super(context, resource, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Course course = getItem(position);
+
+        LayoutInflater inflater  = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.course_record, parent, false);
+
+        TextView textViewCode, textViewTitle, textViewCredit;
+
+        textViewCode = (TextView)rowView.findViewById(R.id.textViewCode);
+        textViewTitle = (TextView)rowView.findViewById(R.id.textViewTitle);
+        textViewCredit = (TextView)rowView.findViewById(R.id.textViewCredit);
+
+        textViewCode.setText(textViewCode.getText() + ":" +course.getCode());
+        textViewTitle.setText(textViewTitle.getText() + ":" + course.getTitle());
+        textViewCredit.setText(textViewCredit.getText() + ":" + course.getCredit());
+        return rowView;
+    }
+}
